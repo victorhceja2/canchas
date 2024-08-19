46792cf67bfdd628744a500d9a69d18522a8380b
Victor Ceja <victorhceja@hotmail.com>
2024-08-19 12:02:58 -0600

    cambio credenciales del servidor de bd

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -4,10 +4,10 @@ if(!defined('DB_SERVER')){
 }
 class DBConnection{
 
-    private $host = DB_SERVER;
-    private $username = DB_USERNAME;
-    private $password = DB_PASSWORD;
-    private $database = DB_NAME;
+    private $host = mariadb-container;
+    private $username = root;
+    private $password = mypassword;
+    private $database = mydatabase;
     
     public $conn;
     
